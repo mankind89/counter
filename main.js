6a9d842d170b72eebedc888e8a4e2769ae1dc6ce
mankind89 <hameeddominic295@gmail.com>
2023-08-07 06:36:09 -0700

    done

---
+++ main.js
@@ -0,0 +1,40 @@
+
+let count =0;
+const increaseBtn = document.querySelector("#increase");
+const decreaseBtn = document.querySelector("#decrease");
+
+function displayCounter() {
+  count ++;
+ document.getElementById('count').innerHTML =count
+ };
+
+ function decreaseCounter() {
+  count--;
+ document.getElementById('count').innerHTML =count
+ };
+decreaseBtn.addEventListener("click",()=> {
+  decreaseCounter()
+
+});
+increaseBtn.addEventListener('click', () => {
+ displayCounter() 
+})
+
+
+/*let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+   sum = sum + 5;
+   
+}
+
+// Only change code above this line
+
+addThree();
+addFive();*/
+
